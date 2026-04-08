d25d189d797dde9d3d8575bfe7d8d8512886372c
i-niranjan <niranjanchaudhari2004@gmail.com>
2026-04-08 22:45:47 +0530

    added walrus

---
+++ fast_learn/index.py
@@ -0,0 +1,14 @@
+# name = "niranjan"
+# age = 23
+# am_i_learning_a_python = True
+
+# print(name)
+# print(age)
+# print(type(am_i_learning_a_python))
+
+user = {
+    "name" : "Niranjan Chaudhari",
+    "age" :  23,
+    "skills": ["JS", "TS"]
+}
+print(user)
