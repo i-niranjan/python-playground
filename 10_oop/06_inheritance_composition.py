8a09eff09d332fd8e2a5675751ee919f8b6aa2d5
i-niranjan <niranjanchaudhari2004@gmail.com>
2026-06-16 09:03:55 +0530

    base class | inheritance and composition

--- 10_oop/06_inheritance_composition.py
+++ 10_oop/06_inheritance_composition.py
@@ -10,18 +10,33 @@ class MasalaChai(BaseChai):
     def add_spices(self):
         print("Adding cardamon, ginger, cloves")
 
-chai = MasalaChai("Masala")
-
-chai.add_spices()
 
 
 ## composition
 
 class ChaiShop:
-    chai_clas = BaseChai
+    chai_cls = BaseChai
 
     def __init__(self):
-        self.chai = self.chai_clas("Regular")
+        self.chai = self.chai_cls("Regular")
+
+    def serve(self):
+        self.chai.prepare()
+        print(f"Serving {self.chai.type} chai in the shop")
+
+
+class FancyChaiShop(ChaiShop):
+    chai_cls = MasalaChai
+
+
+
+fancy = FancyChaiShop()
+
+
+fancy.chai_cls.add_spices(fancy.chai)
+# fancy.chai.add_spices()
+fancy.serve()
+
 
 
 
